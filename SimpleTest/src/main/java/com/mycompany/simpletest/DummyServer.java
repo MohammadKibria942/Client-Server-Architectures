dc32165a09d2d51474cccbe863ff2ab3952cda8c
MohammadKibria942 <yusefkibria942@gmail.com>
2023-03-17 21:29:00 +0000

    Week 1 Updated

--- SimpleTest/src/main/java/com/mycompany/simpletest/DummyServer.java
+++ SimpleTest/src/main/java/com/mycompany/simpletest/DummyServer.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.simpletest;
 
 import java.util.Date;
 
-/**
- *
- * @author lolbo
- */
 public class DummyServer {
     
     String dummyServerName = "UoW_SERVER";
